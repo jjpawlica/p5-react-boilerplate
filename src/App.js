1f4f2d58aee47e8fca554fd2ccf391a3c0b257c6
Jakub Pawlica <jakub.pawlica@gmail.com>
2019-01-03 17:15:15 +0100

    Added more complicated example

--- src/App.js
+++ src/App.js
@@ -2,13 +2,126 @@ import React, { Component } from 'react';
 import P5Wrapper from './components/P5Wrapper';
 import example from './sketches/example';
 
+import './App.css';
+
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      isPainting: true,
+      isRandom: true,
+      dots: 0,
+      maxDots: 2000,
+      red: 255,
+      green: 255,
+      blue: 255,
+      shouldSketchReset: false
+    };
+  }
+
   updateStateHandler = (newState, callback) => this.setState(newState, callback);
 
+  changeMaxDots = event => {
+    this.setState({ maxDots: event.target.value });
+  };
+
+  changeRedValue = event => {
+    this.setState({ red: event.target.value });
+  };
+
+  changeGreenValue = event => {
+    this.setState({ green: event.target.value });
+  };
+
+  changeBlueValue = event => {
+    this.setState({ blue: event.target.value });
+  };
+
+  toggleRandomColor = () => {
+    const { isRandom } = this.state;
+    this.setState({ isRandom: !isRandom, red: 255, green: 255, blue: 255 });
+  };
+
+  togglePaining = () => {
+    const { isPainting } = this.state;
+    this.setState({ isPainting: !isPainting });
+  };
+
+  resetSketch = () => {
+    const { shouldSketchReset } = this.state;
+    this.setState({ dots: 0, isPainting: true, shouldSketchReset: !shouldSketchReset });
+  };
+
   render() {
+    const { red, green, blue, maxDots, isPainting, isRandom, shouldSketchReset, dots } = this.state;
     return (
-      <div>
-        <P5Wrapper sketch={example} updateStateHandler={this.updateStateHandler} />
+      <div className="container">
+        <h1 className="header">p5 React Wrapper</h1>
+        <div className="canvas">
+          <P5Wrapper
+            sketch={example}
+            sketchValues={{ red, green, blue, maxDots, isPainting, isRandom, shouldSketchReset }}
+            updateStateHandler={this.updateStateHandler}
+          />
+        </div>
+        <div className="control">
+          <h2>{`Dots painted: ${dots}`}</h2>
+          <label htmlFor="randomColor">
+            Random color:
+            <input
+              type="checkbox"
+              id="randomColor"
+              value={isRandom}
+              onChange={this.toggleRandomColor}
+            />
+          </label>
+          <br />
+          <label htmlFor="redValue">
+            Red:
+            <input
+              type="range"
+              id="redValue"
+              value={red}
+              min="0"
+              max="255"
+              step="1"
+              onChange={this.changeRedValue}
+            />
+          </label>
+          <br />
+          <label htmlFor="greenValue">
+            Green:
+            <input
+              type="range"
+              id="greenValue"
+              value={green}
+              min="0"
+              max="255"
+              step="1"
+              onChange={this.changeGreenValue}
+            />
+          </label>
+          <br />
+          <label htmlFor="blueValue">
+            Blue:
+            <input
+              type="range"
+              id="blueValue"
+              value={blue}
+              min="0"
+              max="255"
+              step="1"
+              onChange={this.changeBlueValue}
+            />
+          </label>
+          <br />
+          <button type="button" onClick={this.togglePaining} disabled={dots >= maxDots}>
+            {!isPainting ? 'START' : 'STOP'}
+          </button>
+          <button type="button" onClick={this.resetSketch} disabled={isPainting}>
+            RESET
+          </button>
+        </div>
       </div>
     );
   }
--- src/sketches/example.js
+++ src/sketches/example.js
@@ -1,15 +1,69 @@
 /* eslint-disable no-param-reassign */
 
 const sketch = p => {
-  p.redrawHandler = sketchValues => {};
+  let x = 100;
+  let y = 100;
+
+  let red;
+  let green;
+  let blue;
+
+  let isPainting;
+  let isRandom;
+  let shouldSketchReset;
+
+  let maxDots;
+  let dots = 0;
+
+  p.redrawHandler = sketchValues => {
+    ({ red, green, blue, maxDots, isPainting, isRandom, shouldSketchReset } = sketchValues);
+  };
+
+  p.resetSketch = resetSketch => {
+    dots = 0;
+    p.clear();
+    p.setup();
+  };
 
   p.setup = () => {
-    p.createCanvas(window.innerWidth, window.innerHeight);
+    p.createCanvas(600, 600);
     p.background(51);
     p.frameRate(60);
   };
 
-  p.draw = () => {};
+  p.draw = () => {
+    if (shouldSketchReset) {
+      p.resetSketch();
+      p.updateStateHandler({ dots, shouldSketchReset: false });
+    }
+    if (isPainting) {
+      p.fill(red, green, blue, 100);
+      p.noStroke();
+
+      x = p.random(0, 600);
+      y = p.random(0, 600);
+
+      p.ellipse(x, y, 32, 32);
+
+      dots += 1;
+
+      p.updateStateHandler({
+        dots
+      });
+
+      if (isRandom) {
+        p.updateStateHandler({
+          red: p.random(255),
+          green: p.random(255),
+          blue: p.random(255)
+        });
+      }
+
+      if (dots >= maxDots) {
+        p.updateStateHandler({ isPainting: false });
+      }
+    }
+  };
 };
 
 export default sketch;
